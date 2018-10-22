0878483d43227e13dec08cb9ccdc5b5d92e5091d
super-nexus <androidxl.news.xl@gmail.com>
2018-10-23 01:07:28 +0200

    Added acp functionality

--- gitAdder.js
+++ gitAdder.js
@@ -18,7 +18,16 @@ const argv = yargs
         path: pathConfig
     })
     .command('rm', 'Removes the file from specified file path', {
+        //USE AT YOUR OWN RISK
         path: pathConfig
+    })
+    .command('acp', "Adds commits and pushes the directory to origin master", {
+        path: pathConfig,
+        message: {
+            demand: true,
+            alias: 'm',
+            describe: "Message for the git commit"
+        }
     })
     .help().argv;
 
@@ -36,7 +45,7 @@ var createPaths = (ignored, path) => {
     return holder;
 }
 
-var addToGit = path => {
+var addToGit = (path, addIgnore) => {
     return new Promise((resolve, reject) => {
 
         const gitIgnore = './.gitignore';
@@ -44,11 +53,11 @@ var addToGit = path => {
         // (!fs.exists(gitIgnore))
         //     fs.writeFile(gitIgnore, createPaths(ignoredFiles, path), err => console.log(err));
 
-
-        var currentConfig = fs.readFileSync(gitIgnore);
-        currentConfig += createPaths(ignoredFiles, path);
-        fs.writeFileSync(gitIgnore, currentConfig, err => console.log(err));
-
+        if (addIgnore) {
+            var currentConfig = fs.readFileSync(gitIgnore);
+            currentConfig += createPaths(ignoredFiles, path);
+            fs.writeFileSync(gitIgnore, currentConfig, err => console.log(err));
+        }
 
         if (shell.exec('git add ' + argv.path).code !== 0) {
             reject('Error adding file to git repository');
@@ -60,11 +69,56 @@ var addToGit = path => {
     });
 }
 
+var commitGit = (message) => {
+    return new Promise((resolve, reject) => {
+        if (shell.exec('git commit -m "' + message + '"').code !== 0) {
+            reject("Failed to commit changes");
+        }
+        else {
+            resolve("Commited changes");
+        }
+    })
+}
+
+var pushGit = () => {
+    return new Promise((resolve, reject) => {
+        if (shell.exec('git push origin master').code !== 0) {
+            reject("Failed to push to origin master");
+        }
+        else {
+            resolve("Pushed to master");
+        }
+    })
+}
+
 if (command == 'addDir') {
     console.log('Adding directory: ', argv.path);
 
-    addToGit(argv.path).then(message => console.log(message)).catch(err => console.log(err));
+    addToGit(argv.path, true).then(message => console.log(message)).catch(err => console.log(err));
 
+}
+else if (command == 'acp') {
+
+    console.log('ACP started');
+
+    var path = argv.path
+    var addToIgnore = false;
+
+    if(fs.exists(path + ignoredFiles[0])){
+        addToIgnore = true;
+    }
+
+    addToGit(path, addToIgnore).then(message => {
+        console.log(message);
+        commitGit(argv.message).then(commitMessage => {
+            console.log(commitMessage);
+            pushGit().then(pushMessage => {
+                console.log(pushMessage);
+            })
+        });
+    }).catch(err => {
+        console.log(err);
+    })
 
 }
 
