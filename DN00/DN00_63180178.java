352566391141ab8de3e81b11d4d4d5274249290a
super-nexus <androidxl.news.xl@gmail.com>
2018-10-23 12:00:33 +0200

    added dn 00

---
+++ DN00/DN00_63180178.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DN00_63180178{
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int numberOfCandy = sc.nextInt();
+        int numberOfPeople = sc.nextInt();
+
+        int candyPerPerson = numberOfCandy / numberOfPeople;
+
+        System.out.println(candyPerPerson);
+        System.out.println(numberOfCandy - candyPerPerson);
+
+    }
+
+}
