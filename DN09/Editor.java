7eaf5c081d58d5a3b1d0882ac0586a038dab27b3
super-nexus <androidxl.news.xl@gmail.com>
2019-01-08 14:10:01 +0100

    Added DN09 folder

---
+++ DN09/Editor.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Editor{
+
+    private static final String hashtag = "#";
+    private static final String undo = "<";
+    private static final String redo = ">";
+    private static final String add = "+";
+    private static final String remove = "-";
+
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> listOFCommands = new ArrayList<String>();
+        ArrayList<String> text = new ArrayList<String>();
+
+        while(sc.hasNext()){
+
+
+
+        }
+
+    }
+
+
+
+
+}
